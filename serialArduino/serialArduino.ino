0370d9c9724ad211558d3249a0819a90ac671ce1
Hugo <hshugoricardosilva@gmail.com>
2019-06-12 23:10:24 -0300

    add files comunication in serial

--- serialArduino/serialArduino.ino
+++ serialArduino/serialArduino.ino
@@ -1,6 +1,6 @@
 const byte DATA_MAX_SIZE = 32;
 char data[DATA_MAX_SIZE]; 
-
+String incoming = "";
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600); 
@@ -8,8 +8,49 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("hello from arduino!");
-  receiveData();
+  Serial.println("hello from arduino!");  
+
+  static char endMarker = '\n'; // message separator
+  char receivedChar;     // read char from serial port
+  int ndx = 0;          // current index of data buffer
+  // clean data buffer
+  
+
+  //memset(data, 10, sizeof(data));
+  // read while we have data available and we are
+  // still receiving the same message.
+  //while(Serial.available() > 0) {
+     incoming = Serial.readString();
+    Serial.println(incoming);
+    /*
+    receivedChar = Serial.read();
+     //Serial.println("DEV"+receivedChar );
+    if (receivedChar == endMarker) {
+      data[ndx] = '\0'; // end current message
+      return;
+    }
+    // looks like a valid message char, so append it and
+    // increment our index
+    data[ndx] = receivedChar;
+    ndx++;
+    // if the message is larger than our max size then
+    // stop receiving and clear the data buffer. this will
+    // most likely cause the next part of the message
+    // to be truncated as well, but hopefully when you
+    // parse the message, you'll be able to tell that it's
+    // not a valid message.
+    if (ndx >= DATA_MAX_SIZE) {
+      break;
+    }
+    */
+  //}
+  // no more available bytes to read from serial and we
+  // did not receive the separato. it's an incomplete message!
+  //Serial.println("error: incomplete message");
+ // memset(data, 10, sizeof(data));
+  
+  
+  //receiveData();
   delay(1000);
 }
 
@@ -19,11 +60,16 @@ void receiveData() {
   char receivedChar;     // read char from serial port
   int ndx = 0;          // current index of data buffer
   // clean data buffer
+  
+
   memset(data, 32, sizeof(data));
   // read while we have data available and we are
   // still receiving the same message.
   while(Serial.available() > 0) {
+   
+    
     receivedChar = Serial.read();
+     Serial.println(receivedChar);
     if (receivedChar == endMarker) {
       data[ndx] = '\0'; // end current message
       return;
@@ -45,6 +91,5 @@ void receiveData() {
   // no more available bytes to read from serial and we
   // did not receive the separato. it's an incomplete message!
   Serial.println("error: incomplete message");
-  Serial.println(data);
   memset(data, 32, sizeof(data));
 }
--- webserver_serial.js
+++ webserver_serial.js
@@ -1,22 +1,39 @@
 const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
-const port = new SerialPort('/dev/ttyACM0', { baudRate: 9600 });
-//const parser = port.pipe(new Readline({ delimiter: '\n' }));
+const port = new SerialPort('COM3', { baudRate: 9600 });
+const parser = port.pipe(new Readline({ delimiter: '\n' }));
 // Read the port data
 port.on("open", () => {
   console.log('serial port open');
 });
-/*
-parser.on('data', data =>{
+
+parser.on('data', data => {
   console.log('got word from arduino:', data);
 });
-*/
+
 
 //Enviamos a Arduino:
-
+/*
 port.write('hello from node\n', (err) => {
 	if (err) {
 	  return console.log('Error on write: ', err.message);
 	}
 	console.log('message written');
+});*/
+
+port.write('AA', (err) => {
+	if (err) {
+	  return console.log('Error on write: ', err.message);
+	}
+	console.log('message written');
 });
+
+function write (){
+	port.write('BB', (err) => {
+		if (err) {
+		  return console.log('Error on write: ', err.message);
+		}
+		console.log('message written');
+	});
+}
+setInterval(write, 2000);
