783ef3bf88a73a5781cec3382796181056931cab
Hugo Silva <hshugoricardosilva@gmail.com>
2019-06-20 21:30:15 -0300

    correct arduino

--- arduino/2_motores_Web/2_motores_Web.ino
+++ arduino/2_motores_Web/2_motores_Web.ino
@@ -16,7 +16,7 @@
 int retardo = 5;        // Tiempo de retardo en milisegundos (Velocidad del Motor)
 int dato_rx;            // valor recibido en grados
 int numero_pasos = 0;   // Valor en grados donde se encuentra el motor
-char mover="";
+char mover=' ';
 String leeCadena;       // Almacena la cadena de datos recibida
 
 void setup() {
@@ -36,8 +36,8 @@ void setup() {
 
 void loop() {
 
-  int j=0;
-  mover=" ";
+   int j=0;
+  mover=' ';
   while (Serial.available()) {    // Leer el valor enviado por el Puerto serial
 
     if(j==0){
@@ -46,38 +46,36 @@ void loop() {
       delay(retardo);
       char c  = Serial.read();     // Lee los caracteres
       Serial.print(" QUE ES? ");
-      if(mover!=' ' && (c=='L' || c=='R' || c=='U' || c=='D') ){ 
+      if(mover==' ' && (c=='L' || c=='R' || c=='U' || c=='D') ){ 
         Serial.print(" ALGO ");
         mover=c;
       }else{
-        /*
         if(c!='-'){
-          leeCadena += c;              // Usa los numeros
+          leeCadena += c;              // Convierte Caracteres a cadena de caracteres
         }
-        */
       }
     }   
     j++;
   }
-  /*
   if (leeCadena.length() > 0) {
     dato_rx = leeCadena.toInt();   // Convierte Cadena de caracteres a Enteros
-    Serial.print(dato_rx);         // Envia valor en Grados
+    Serial.print(mover);         // Envia valor en Grados
     Serial.println(" Grados");
     delay(retardo);
     dato_rx = (dato_rx * 1.4222222222); // Ajuste de 512 vueltas a los 360 grados
   }
-  */
   dato_rx =15;
   numero_pasos =0;
   while (mover=='L' && dato_rx > numero_pasos) { // Girohacia la izquierda en grados
     Serial.print("izquierda\n");
     motor1_IZQ();
+   // motor2_ABA();
     numero_pasos = numero_pasos + 1;
   }
 
   while (mover=='D' && dato_rx > numero_pasos) { // Girohacia la izquierda en grados
     Serial.print("DOWN\n");
+    //motor1_IZQ();
     motor2_ABA();
     numero_pasos = numero_pasos + 1;
   }
@@ -85,16 +83,18 @@ void loop() {
   while (mover=='R' && dato_rx > numero_pasos) { // Giro hacia la derecha en grados
     Serial.print("derecha\n");
     motor1_DER();
+   // motor2_ARR();
     numero_pasos = numero_pasos + 1;
   }
 
   while (mover=='U' && dato_rx > numero_pasos) { // Giro hacia la derecha en grados
     Serial.print("UP\n");
+    //motor1_DER();
     motor2_ARR();
     numero_pasos = numero_pasos + 1;
   }
   
-  //leeCadena = "";   // Inicializamos la cadena de caracteres recibidos
+  leeCadena = "";   // Inicializamos la cadena de caracteres recibidos
   apagado();         // Apagado del Motor para que no se caliente
 }  ///////////////////// Fin del Loop ///////////////////////////
 
@@ -156,18 +156,18 @@ void motor2_ARR() {
   digitalWrite(4, HIGH);
   delay(retardo);
   digitalWrite(7, LOW);
-  digitalWrite(6, LOW);
+  digitalWrite(6, HIGH);
   digitalWrite(5, HIGH);
-  digitalWrite(4, HIGH);
+  digitalWrite(4, LOW);
   delay(retardo);
-  digitalWrite(7, LOW);
-  digitalWrite(6, LOW);
-  digitalWrite(5, HIGH);
-  digitalWrite(4, HIGH);
+  digitalWrite(7, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(5, LOW);
+  digitalWrite(4, LOW);
   delay(retardo);
-  digitalWrite(7, LOW);
+  digitalWrite(7, HIGH);
   digitalWrite(6, LOW);
-  digitalWrite(5, HIGH);
+  digitalWrite(5, LOW);
   digitalWrite(4, HIGH);
   delay(retardo);
   
